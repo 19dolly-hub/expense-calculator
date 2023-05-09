e4b4d79d9e40d51c658db4a493c4055e38894aa4
dolly <dollyraghuwanshi16@gmail.com>
2023-05-10 00:53:27 +0530

    lecture7: templating

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
         // getting all elements
         let amtEl = document.querySelector('#amt');
-        const inputEl = document.querySelector('#inputExpense');
         const spentEl = document.querySelector('#inputDesc');  
+        const inputEl = document.querySelector('#inputExpense');
         let btnEl = document.querySelector('#addBtn');
+        let tableEl = document.querySelector('.expenseTable');
 
         // starting from zero
         let totalExpense = 0;
@@ -25,19 +26,29 @@
            //putting the info in the expenseItems object
            expenseItems.desc = descText;
            expenseItems.amt = amtText;
-           console.log( expenseItems);      
            
            // pushing the data in the allExpenses array
            allExpenses.push(expenseItems);
-
-           console.clear(); // To get only final table
-           console.table(allExpenses); // To see in tabular form
            
            //add that value to totalExpense
            totalExpense = totalExpense + expense;    
 
             // setting the amount element to totalExpense
-            amtEl.textContent = totalExpense;                }
+            amtEl.textContent = totalExpense;
+            
+            // making table for only two list items
+            const data1 = allExpenses[0];
+            const data2 = allExpenses[1];
+
+            const data1Text = `${data1.desc} : ${data1.amt}`;
+            const data2Text = `${data2.desc} : ${data2.amt}`;
+            
+            const theTable = `
+            <div>${data1Text}</div>
+            <div>${data2Text}</div>
+            `;
+            tableEl.innerHTML = theTable;
+        }
 
         // listening click event
         btnEl.addEventListener('click', addExpenseToTotal, false); // why it is false
